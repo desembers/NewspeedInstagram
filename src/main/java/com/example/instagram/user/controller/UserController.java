48f7fe8917705f910b18d665f62048db6465b6e4
ehy0419 <2hy0419@naver.com>
2025-08-19 17:14:08 +0900

    fix.UserController 에 private final ProfileService profileService; 필드 추가

--- src/main/java/com/example/instagram/user/controller/UserController.java
+++ src/main/java/com/example/instagram/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.instagram.user.controller;
 
 import com.example.instagram.common.consts.Const;
 import com.example.instagram.profile.dto.response.ProfileResponseDto;
+import com.example.instagram.profile.service.ProfileService;
 import com.example.instagram.user.dto.request.UserSaveRequestDto;
 import com.example.instagram.user.dto.request.UserUpdateRequestDto;
 import com.example.instagram.user.dto.response.UserResponseDto;
@@ -18,6 +19,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final ProfileService profileService;
 
     /// 회원가입 USERS 리소스 생성
     /// 사용자 인증 회원가입 - "/auth/signup"
@@ -34,20 +36,20 @@ public class UserController {
     }
 
     // 개별 유저 상세
-    @GetMapping("/users/{usersid}")
+    @GetMapping("/users/{userId}")
     public ResponseEntity<UserResponseDto> findOne(
-            @PathVariable Long userid) {                            // 경로 플레이스홀더 이름이 'usersid'이므로
-                                                                    // 명시적으로 매핑해 400/404 매핑 오류를 방지
-        return ResponseEntity.ok(userService.findOne(userid));      // 컨트롤러는 조회 위임만, 변환/검증은 서비스
+            @PathVariable Long userId
+    ) {
+        return ResponseEntity.ok(userService.findOne(userId));
     }
 
-    /// 프로필 조회: GET /users/{usersid}/profiles  수정!!  version
-//    @GetMapping("/users/{usersid}/profiles")
-//    public ResponseEntity<ProfileResponseDto> findProfile(
-//            @PathVariable("usersid") Long userId // 왜: 경로 변수명과 정확히 일치시켜 매핑
-//    ) {
-//        return ResponseEntity.ok(profileService.findByUserId(userId));
-//    }
+    /// 프로필 조회: GET /users/{usersId}/profiles
+    @GetMapping("/users/{userId}/profiles")
+    public ResponseEntity<ProfileResponseDto> findProfile(
+            @PathVariable Long userId
+    ) {
+        return ResponseEntity.ok(profileService.findByUserId(userId));
+    }
 
     // 본인 정보 수정(전체 교체 의미로 PUT 선택)
     @PutMapping("/users/me")
