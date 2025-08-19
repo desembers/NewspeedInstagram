67c407212bdd339819a9a5854e84bac9b16ca91d
hailee2 <slowslow9150@gmail.com>
2025-08-19 14:56:21 +0900

    Clean up: 사용하지않는 임포트 삭제

--- src/main/java/com/example/instagram/newsFeeds/Repository/NewsFeedRepository.java
+++ src/main/java/com/example/instagram/newsFeeds/Repository/NewsFeedRepository.java
@@ -4,7 +4,6 @@ import com.example.instagram.newsFeeds.entity.NewsFeed;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface NewsFeedRepository extends JpaRepository<NewsFeed,Long> {
     Page<NewsFeed> findAllByOrderByCreatedAtDesc (Pageable pageable);
