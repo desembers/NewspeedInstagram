807c63d665d616ebb378ccf48dc11517e85873e5
hailee2 <slowslow9150@gmail.com>
2025-08-20 12:14:12 +0900

    [newsfeeds]중복클래스 제거 및 modifiedAt -> updatedAt 수정

--- src/main/java/com/example/instagram/newsFeeds/dto/NewsFeedGetResponse.java
+++ src/main/java/com/example/instagram/newsFeeds/dto/NewsFeedGetResponse.java
@@ -9,19 +9,19 @@ public class NewsFeedGetResponse {
     private final Long authorId;
     private final String content;
     private final LocalDateTime createdAt;
-    private final LocalDateTime modifiedAt;
+    private final LocalDateTime updatedAt;
 
     public NewsFeedGetResponse(
             Long id,
             Long authorId,
             String content,
             LocalDateTime createdAt,
-            LocalDateTime modifiedAt)
+            LocalDateTime updatedAt)
     {
         this.id = id;
         this.authorId = authorId;
         this.content = content;
         this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
+        this.updatedAt = updatedAt;
     }
 }
