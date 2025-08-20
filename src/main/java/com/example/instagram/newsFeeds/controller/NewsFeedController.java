0d70abfec9a66ff241f46cc19ee35094c811601e
cjn404 <moi404@naver.com>
2025-08-20 21:50:09 +0900

    [Auth] feat: AuthUser 객체를 생성하고 @Auth 어노테이션으로 주입

--- src/main/java/com/example/instagram/newsFeeds/controller/NewsFeedController.java
+++ src/main/java/com/example/instagram/newsFeeds/controller/NewsFeedController.java
@@ -1,5 +1,7 @@
 package com.example.instagram.newsFeeds.controller;
 
+import com.example.instagram.auth.annotation.Auth;
+import com.example.instagram.auth.dto.AuthUser;
 import com.example.instagram.newsFeeds.dto.*;
 import com.example.instagram.newsFeeds.service.NewsFeedService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,10 +20,10 @@ public class NewsFeedController {
     @PostMapping("/newsFeeds")
     public ResponseEntity<NewsFeedSaveResponse> saveNewsFeed(
             @RequestBody NewsFeedSaveRequest request,
-            @AuthenticationPrincipal CustomUserDetails userDetails // Security에서 로그인한 사용자
+            @Auth AuthUser authUser // Security에서 로그인한 사용자
     ){
-        //Long testUserId = 1L; // 임시 테스트용 유저 ID
-        return ResponseEntity.ok(newsFeedService.save(request,userDetails.getUser().getId()));
+        Long testUserId = 1L; // 임시 테스트용 유저 ID
+        return ResponseEntity.ok(newsFeedService.save(request, authUser.getId()));    // AuthUser 객체를 생성하고 @Auth 어노테이션으로 주입
     }
 
     @GetMapping("/newsFeeds")
