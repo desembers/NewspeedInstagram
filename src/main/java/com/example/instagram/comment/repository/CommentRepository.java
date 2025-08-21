f7bbd38892ad6c77b57a7bd215e7f32779c1a0c7
ehy0419 <2hy0419@naver.com>
2025-08-21 20:30:42 +0900

    fix. long -> Long 자료형 통일

--- src/main/java/com/example/instagram/comment/repository/CommentRepository.java
+++ src/main/java/com/example/instagram/comment/repository/CommentRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByUserId(long userId);
+    List<Comment> findByUserId(Long userId);
 }
