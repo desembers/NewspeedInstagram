54a571e5c87c9410988a357362251e2af6570d5f
ehy0419 <2hy0419@naver.com>
2025-08-22 10:55:34 +0900

    feat. 레포지토리 수정_유저id로 프로필 조회및검증, 삭제

--- src/main/java/com/example/instagram/profile/repository/ProfileRepository.java
+++ src/main/java/com/example/instagram/profile/repository/ProfileRepository.java
@@ -5,7 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProfileRepository extends JpaRepository<Profile, Long> {       // PK가 user_id 이므로 타입은 LONG
-    Optional<Profile> findByUserId(Long userId);                                // 사용자가 기준으로 프로필 조회
-    boolean existsByUserId(Long userId);                                        // 중복 생성 방지
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    // PK가 user_id 이므로 타입은 LONG
+
+    // 프로필 조회 및 검증 - 사용자 Id 프로필 조회
+    boolean existsByUserId(Long userId);       // 중복 생성 방지
+    Optional<Profile> findByUserId(Long userId);
+
+    // 프로필 삭제
+    void deleteByUserId(Long userId);
 }
