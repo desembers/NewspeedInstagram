da8bff29e44a64887790ada249846884ab06173b
ehy0419 <2hy0419@naver.com>
2025-08-22 23:51:02 +0900

    fix. userName

--- src/main/java/com/example/instagram/user/dto/response/UserResponseDto.java
+++ src/main/java/com/example/instagram/user/dto/response/UserResponseDto.java
@@ -9,18 +9,18 @@ public class UserResponseDto {
 
     // 응답 전용 불변 DTO로 엔티티 누출 방지 + 직렬화 안정성
     private final Long id;
-    private final String username;
+    private final String userName;
     private final String email;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
     // MapperClass..?, 정적 메서드 from, of, 정적 팩토리 메서드..
     public UserResponseDto(Long id,
-                           String username, String email,
+                           String userName, String email,
                            LocalDateTime createdAt, LocalDateTime updatedAt
     ) {
         this.id = id;
-        this.username = username;
+        this.userName = userName;
         this.email = email;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
