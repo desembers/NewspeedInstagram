a0dfeb1154aa191db2def904d8e29a394a4d70e5
cjn404 <moi404@naver.com>
2025-08-24 19:25:14 +0900

    [Profile] feat: 예외 처리 HTTP 에러 코드 추가

--- src/main/java/com/example/instagram/profile/service/ProfileService.java
+++ src/main/java/com/example/instagram/profile/service/ProfileService.java
@@ -1,6 +1,5 @@
 package com.example.instagram.profile.service;
 
-import com.example.instagram.common.exception.InValidException;
 import com.example.instagram.profile.dto.request.ProfileSaveRequestDto;
 import com.example.instagram.profile.dto.request.ProfileUpdateRequestDto;
 import com.example.instagram.profile.dto.response.ProfileResponseDto;
@@ -9,8 +8,10 @@ import com.example.instagram.profile.repository.ProfileRepository;
 import com.example.instagram.user.entity.User;
 import com.example.instagram.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -24,11 +25,11 @@ public class ProfileService {
     @Transactional
     public ProfileResponseDto create(Long userId, ProfileSaveRequestDto dto) {
         if (profileRepository.existsByUserIdAndDeletedFalse(userId)) {             // 프로필과 유저는 1:1 (공유)구조, 프로필 중복 생성 방지
-            throw new IllegalArgumentException("이미 프로필이 존재합니다.");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 프로필이 존재합니다.");    // 에러 코드 409
         }
 
         User user = userRepository.findById(userId)                 // 공유 PK(MapsId)라 실제 User가 있어야 저장 가능
-                .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "유저가 존재하지 않습니다."));    // 에러 코드 404
 
         Profile profile = new Profile(
                 user,
@@ -41,7 +42,7 @@ public class ProfileService {
 
         //비밀번호가 일치하지 않으면 다시 로그인해주세요 (브라우저가 임의로 비밀번호 치는것 방지)
         if (!profile.getUser().getPassword().equals(user.getPassword())) {
-            throw new InValidException("비밀번호가 일치하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");     // 에러 코드 401
         }
         return toDto(profile);
     }
@@ -49,14 +50,14 @@ public class ProfileService {
     @Transactional(readOnly = true)
     public ProfileResponseDto findByUserId(Long userId) {
         Profile profile = profileRepository.findByUserIdAndDeletedFalse(userId)
-                .orElseThrow(() -> new IllegalArgumentException("프로필이 존재하지 않습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."));    // 에러 코드 404
         return toDto(profile);
     }
 
     @Transactional
     public ProfileResponseDto update(Long userId, ProfileUpdateRequestDto dto) {
         Profile profile = profileRepository.findByUserIdAndDeletedFalse(userId)
-                .orElseThrow(() -> new IllegalArgumentException("프로필이 존재하지 않습니다."));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."));    // 에러 코드 404
         profile.update(
                 dto.getDisplayName(),
                 dto.getBio(),
@@ -71,7 +72,7 @@ public class ProfileService {
     public void deleteByUserId(Long userId) {
         // 프로필 객체 조회
         Profile profile = profileRepository.findByUserIdAndDeletedFalse(userId).orElseThrow(
-                () -> new IllegalArgumentException("프로필이 존재하지 않습니다."));
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "프로필이 존재하지 않습니다."));    // 에러 코드 404
 
         // Soft Delete 적용
         profile.softDelete();
