a7a2694478671ed0260455cc103fbbfbc64e611c
cjn404 <moi404@naver.com>
2025-08-24 14:53:34 +0900

    [User] style: username -> userName 수정

--- src/main/java/com/example/instagram/user/dto/response/UserResponseDto.java
+++ src/main/java/com/example/instagram/user/dto/response/UserResponseDto.java
@@ -9,17 +9,17 @@ public class UserResponseDto {
 
     // 응답 전용 불변 DTO로 엔티티 누출 방지 + 직렬화 안정성
     private final Long id;
-    private final String username;
+    private final String userName;
     private final String email;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
 
     public UserResponseDto(Long id,
-                           String username, String email,
+                           String userName, String email,
                            LocalDateTime createdAt, LocalDateTime updatedAt
     ) {
         this.id = id;
-        this.username = username;
+        this.userName = userName;
         this.email = email;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
